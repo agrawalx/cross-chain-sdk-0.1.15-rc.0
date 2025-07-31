6c65d17282e0918690fef72a6bd3c94bdd25419f
yash <yasha4658@gmail.com>
2025-08-01 00:05:15 +0530

    done

---
+++ src/chains.ts
@@ -0,0 +1,27 @@
+import {NetworkEnum} from '@1inch/fusion-sdk'
+import {TupleToUnion} from './type-utils'
+
+export const SupportedChains = [
+    NetworkEnum.ETHEREUM,
+    NetworkEnum.POLYGON,
+    NetworkEnum.BINANCE,
+    NetworkEnum.OPTIMISM,
+    NetworkEnum.ARBITRUM,
+    NetworkEnum.AVALANCHE,
+    NetworkEnum.GNOSIS,
+    NetworkEnum.COINBASE,
+    NetworkEnum.ZKSYNC,
+    NetworkEnum.LINEA,
+    NetworkEnum.SONIC,
+    NetworkEnum.UNICHAIN
+] as const
+
+type UnsupportedChain = Exclude<
+    NetworkEnum,
+    TupleToUnion<typeof SupportedChains>
+>
+
+export type SupportedChain = Exclude<NetworkEnum, UnsupportedChain>
+
+export const isSupportedChain = (chain: unknown): chain is SupportedChain =>
+    SupportedChains.includes(chain as number)
